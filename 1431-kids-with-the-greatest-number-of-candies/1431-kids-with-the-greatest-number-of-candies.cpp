5dd7219e3912c0222245d2d5869e9f56f44f2cb5
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-04-18 02:12:07 +0600

    Time: 0 ms (100.00%), Space: 8.8 MB (86.17%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxCandies = 0;
+        vector<bool> ans;
+        for(int i = 0; i < candies.size(); i++) {
+            if(maxCandies < candies[i]) {
+                maxCandies = candies[i];
+            }
+        }
+        for(int i = 0; i < candies.size(); i++) {
+            if(maxCandies <= candies[i] + extraCandies) {
+                ans.push_back(true);
+            } else {
+                ans.push_back(false);
+            }
+        }
+        return ans;
+    }
+};
