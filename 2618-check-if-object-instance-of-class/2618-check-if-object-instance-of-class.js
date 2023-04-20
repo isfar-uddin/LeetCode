100cd7a817fbd4b7ecc76cc50d86822031f3167f
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-04-21 01:07:47 +0600

    Time: 97 ms (91.05%), Space: 52.9 MB (29.12%) - LeetHub

---
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -0,0 +1,15 @@
+/**
+ * @param {any} object
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+    if(obj == null) return false;
+    if(obj.constructor === classFunction) return true;
+    obj = Object.getPrototypeOf(obj);
+    return checkIfInstanceOf(obj, classFunction);
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
