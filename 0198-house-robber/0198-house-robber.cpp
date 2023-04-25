09737bfc6095bede3b6571dca4c0d80e75ed9c74
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-04-26 01:40:20 +0600

    Time: 0 ms (100.00%), Space: 7.7 MB (86.79%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dp[101];
+    int rob(vector<int>& nums) {
+        memset(dp, -1, sizeof dp);
+        return findMaxRobbery(nums, 0);
+    }
+    
+    int findMaxRobbery(vector<int>& nums, int num) {
+        if(num >= nums.size()) {
+            return 0;
+        }
+        if(dp[num] > -1) {
+            return dp[num];
+        }
+        int ans = max(findMaxRobbery(nums, num + 1), findMaxRobbery(nums, num + 2) + nums[num]);
+        dp[num] = ans;
+        return ans;
+    }
+};
