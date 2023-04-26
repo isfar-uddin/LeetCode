d282e38b81a4f70e3dcd1118357971e5716106d4
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-04-27 01:58:44 +0600

    Time: 3 ms (56.11%), Space: 5.8 MB (99.93%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
+    // int addDigits(int num) {
+    //     if(num < 10) {
+    //         return num;
+    //     }
+    //     int sum = 0;
+    //     while(num) {
+    //         sum += num % 10;
+    //         num /= 10;
+    //     }
+    //     return addDigits(sum);
+    // }
+    
     int addDigits(int num) {
-        if(num / 10 == 0) {
-            return num;
+        if(num == 0) {
+            return 0;
         }
-        int sum = 0;
-        while(num) {
-            sum += num % 10;
-            num /= 10;
+        if(num % 9 == 0) {
+            return 9;
         }
-        return addDigits(sum);
+        return num % 9;
     }
 };
