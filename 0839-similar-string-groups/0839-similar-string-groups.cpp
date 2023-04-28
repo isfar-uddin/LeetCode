86e7a0e821595138526ac0747d05a00d7b055cdb
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-04-29 05:57:57 +0600

    Time: 188 ms (47.00%), Space: 10.2 MB (59.31%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.cpp
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visit) {
-        visit[node] = true;
-        for (int neighbor : adj[node]) {
-            if (!visit[neighbor]) {
-                dfs(neighbor, adj, visit);
-            }
-        }
-    }
-    
-    bool isSimilar(string& a, string& b) {
-        int diff = 0;
-        for (int i = 0; i < a.size(); i++) {
-            if (a[i] != b[i]) {
-                diff++;
-            }
-        }
-        return diff == 0 || diff == 2;
-    }
-    
     int numSimilarGroups(vector<string>& strs) {
-        int n = strs.size();
+        int n = strs.size(), count = 0;
         vector<vector<int>> adj(n);
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (isSimilar(strs[i], strs[j])) {
+        vector<bool> visited(n);
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(isSimiliar(strs[i], strs[j])) {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
             }
         }
-
-        vector<bool> visit(n);
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (!visit[i]) {
-                dfs(i, adj, visit);
+        
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                dfs(i, adj, visited);
                 count++;
             }
         }
-
         return count;
+    }
+    
+    bool isSimiliar(string& str1, string& str2) {
+        int count = 0;
+        for(int i = 0; i < str1.size(); i++) {
+            if(str1[i] != str2[i]) {
+                count++;
+            }
+        }
+        return count == 0 || count == 2;
+    }
+    
+    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited) {
+        visited[node] = true;
+        for(int i = 0; i < adj[node].size(); i++) {
+            if(!visited[adj[node][i]]) {
+                dfs(adj[node][i], adj, visited);
+            }
+        }
     }
 };
