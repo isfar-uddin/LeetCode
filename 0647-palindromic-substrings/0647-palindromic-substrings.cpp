5729fa666a917a7423b24d26392af65dacf6a2f5
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-05-03 04:51:24 +0600

    Time: 13 ms (48.24%), Space: 7.3 MB (70.33%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int size = s.size();
-        if(size == 1) return 1;
-        int count = size;
-        int dp[size + 1][size + 1];
+        int size = s.size(), count = s.size();
+        bool dp[size + 1][size + 1];
         memset(dp, 0, sizeof dp);
+        
         for(int i = 0; i < size; i++) {
-            dp[i][i] = 1;
+            dp[i][i] = true;
             if(i < size - 1 && s[i] == s[i + 1]) {
-                dp[i][i + 1] = 1;
+                dp[i][i + 1] = true;
                 count++;
             }
         }
+        
         for(int j = 2; j < size; j++) {
             for(int i = 0; i < size - 1; i++) {
-                if(dp[i + 1][j - 1] && s[i] == s[j]) {
-                    dp[i][j] = 1;
+                if(s[i] == s[j] && dp[i + 1][j - 1]) {
+                   dp[i][j] = true;
                     count++;
                 }
             }
