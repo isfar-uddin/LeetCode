b96a111f1bf4d26c9df4d6b8286652382486dc2e
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-05-09 22:26:26 +0600

    Time: 0 ms (100.00%), Space: 6.7 MB (91.37%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -5,44 +5,34 @@ public:
         int colSize = matrix[0].size();
         int left = 0, right = colSize - 1, up = 0, down = rowSize  - 1;
         vector<int> ans;
-        vector<vector<int>> visited(rowSize, vector<int>(colSize, 0));
-        int row = 0, col = 0;
         
         while(ans.size() < rowSize * colSize) {
             for(int i = left; i <= right; i++) {
-                if(visited[row][i]) continue;
-                ans.push_back(matrix[row][i]);
-                visited[row][i] = 1;
+                ans.push_back(matrix[up][i]);
             }
             
             up++;
-            col = right;
             
             for(int i = up; i <= down; i++) {
-                if(visited[i][col]) continue;
-                ans.push_back(matrix[i][col]);
-                visited[i][col] = 1;
+                ans.push_back(matrix[i][right]);
             }
             
             right--;
-            row = down;
             
-            for(int i = right; i >= left; i--) {
-                if(visited[row][i]) continue;
-                ans.push_back(matrix[row][i]);
-                visited[row][i] = 1;
+            if(up <= down) {
+                for(int i = right; i >= left; i--) {
+                    ans.push_back(matrix[down][i]);
+                }
+                down--;
             }
             
-            down--;
-            col = left;
             
-            for(int i = down; i >= up; i--) {
-                if(visited[i][col]) continue;
-                ans.push_back(matrix[i][col]);
-                visited[i][col] = 1;
+            if(left <= right) {
+                for(int i = down; i >= up; i--) {
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
             }
-            left++;
-            row = up;
         }
         
         return ans;
