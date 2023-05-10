966271a476db8cb4889c8bea28fd82764465b36d
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-05-10 08:27:26 +0600

    Time: 68 ms (21.37%), Space: 42.8 MB (32.05%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -6,8 +6,8 @@
  */
 var reduce = function(nums, fn, init) {
     let accumulator = init;
-    for(const num of nums) {
-        accumulator = fn(accumulator, num);
+    for(let i = 0; i < nums.length; i++) {
+        accumulator = fn(accumulator, nums[i]);
     }
     return accumulator;
 };
