073c7ac5e51bdaf5cb5c61f4638185d551a20a03
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-05-11 11:27:28 +0600

    Time: 75 ms (39.41%), Space: 43.2 MB (74.38%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -4,16 +4,19 @@
  */
 var compose = function(functions) {
 	return function(x) {
+        let input = x;
         
-        // for(let i = functions.length - 1; i >= 0; i--) {
-        //     // There will be issue regarding 'this' for function call
-        //     //x = functions[i](x);
-        //     // To resolve this issue we can just bind 'this' to function
-        //     x = functions[i].call(this, x);
-        // }
-        
+        for(let i = functions.length - 1; i >= 0; i--) {
+            // There will be issue regarding 'this' for function call
+            // input = functions[i](input);
+            // To resolve this issue we can just bind 'this' to function
+            input = functions[i].call(this, input);
+        }
+
+        return input;
+
         // We can improve this using reduceRight
-        return functions.reduceRight((acc, func) => func(acc), x);
+        // return functions.reduceRight((acc, func) => func(acc), x);
     }
 };
 
