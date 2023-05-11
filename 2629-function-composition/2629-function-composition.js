6426455e72eca480c3352ec9ecd9662c9af3824c
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-05-11 11:18:40 +0600

    Time: 69 ms (73.58%), Space: 43.2 MB (74.38%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -6,7 +6,10 @@ var compose = function(functions) {
 	return function(x) {
         
         for(let i = functions.length - 1; i >= 0; i--) {
-            x = functions[i](x);
+            // There will be issue regarding 'this' for function call
+            //x = functions[i](x);
+            // To resolve this issue we can just bind 'this' to function
+            x = functions[i].call(this, x);
         }
         
         return x;
