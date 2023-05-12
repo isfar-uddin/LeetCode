a35a45c3765a0830ea96cbac5a11c9ba0539149e
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-05-12 08:35:27 +0600

    Time: 24 ms (48.97%), Space: 10.7 MB (84.20%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<int>& nums1, vector<int>& nums2, vector<vector<int>>& memo) {
-        if (i <= 0 || j <= 0) {
+    int dp[501][501];
+    
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        memset(dp, -1, sizeof dp);
+        return findMaxCount(nums1, nums2, 0, 0);
+    }
+    
+    int findMaxCount(vector<int>& nums1, vector<int>& nums2, int i, int j) {
+        if(i >= nums1.size() || j >= nums2.size()) {
             return 0;
         }
-
-        if (memo[i][j] != -1) {
-            return memo[i][j];
+        
+        if(dp[i][j] != -1) {
+            return dp[i][j];
         }
-
-        if (nums1[i - 1] == nums2[j - 1]) {
-            memo[i][j] = 1 + solve(i - 1, j - 1, nums1, nums2, memo);
+        
+        if(nums1[i] == nums2[j]) {
+            dp[i][j] = 1 + findMaxCount(nums1, nums2, i + 1, j + 1);
         } else {
-            memo[i][j] =
-                max(solve(i, j - 1, nums1, nums2, memo), solve(i - 1, j, nums1, nums2, memo));
+            dp[i][j] = max(findMaxCount(nums1, nums2, i + 1, j), findMaxCount(nums1, nums2, i, j + 1));
         }
-        return memo[i][j];
-    }
-
-    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-
-        vector<vector<int>> memo(n1 + 1, vector<int>(n2 + 1, -1));
-
-        return solve(n1, n2, nums1, nums2, memo);
+        
+        return dp[i][j];
     }
 };
