00cc2578dae85ec08f1674d1ee1990fd46d39df6
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-05-14 23:43:16 +0600

    Time: 246 ms (39.45%), Space: 8.2 MB (70.93%) - LeetHub

---
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int dp[8][16384] = {};
+    
+    int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+    
+    int maxScore(vector<int>& n, int i = 1, int mask = 0) {
+        if (i > n.size() / 2)
+            return 0;
+        if (!dp[i][mask])
+            for (int j = 0; j < n.size(); ++j)
+                for (auto k = j + 1; k < n.size(); ++k) {
+                    int new_mask = (1 << j) + (1 << k);
+                    if ((mask & new_mask) == 0)
+                        dp[i][mask] = max(dp[i][mask], i * gcd(n[j], n[k]) + maxScore(n, i + 1, mask + new_mask));
+                }
+        return dp[i][mask];
+    }
+};
