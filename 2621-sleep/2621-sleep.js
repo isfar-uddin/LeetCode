2a2b3434d48cc70b59c3bdc2c0af7a0b7ed1f5ca
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-05-15 09:49:51 +0600

    Time: 57 ms (70.90%), Space: 41.7 MB (69.70%) - LeetHub

--- 2621-sleep/2621-sleep.js
+++ 2621-sleep/2621-sleep.js
@@ -3,9 +3,7 @@
  */
 async function sleep(millis) {
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve();
-        }, millis);
+        setTimeout(resolve, millis);
     })
 }
 
