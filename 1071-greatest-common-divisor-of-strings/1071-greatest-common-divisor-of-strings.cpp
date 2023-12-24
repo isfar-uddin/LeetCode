6285c0a0201edbb4985b6415111a27b5a279582b
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-12-24 21:36:29 +0600

    Time: 5 ms (48.15%), Space: 7.7 MB (33.97%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
-    int findGCD(int a, int b) {
-        return b == 0 ? a : findGCD(b, a % b);
-    }
-    
-    
-    
     string gcdOfStrings(string str1, string str2) {
-        if(str1 + str2 != str2 + str1) return "";
+        if(str1 + str2 != str2 + str1) {
+            return "";
+        }
         
-        int gcdLength = findGCD(str1.size(), str2.size());
+        int gcdLength = gcd(str1.size(), str2.size());
         
         return str1.substr(0, gcdLength);
     }
