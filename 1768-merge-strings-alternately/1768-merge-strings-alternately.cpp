608012afc4ffd9ecb762b31e210fd5b15a212ec6
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-12-24 16:52:48 +0600

    Time: 0 ms (100.00%), Space: 6.9 MB (22.77%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
+        string mergedString = "";
         int i = 0, j = 0;
-        string ans = "";
         
         while(i < word1.size() && j < word2.size()) {
-            ans += word1[i];
-            ans += word2[j];
+            mergedString += word1[i];
+            mergedString += word2[j];
             i++;
             j++;
         }
         
-        while(i < word1.size()) {
-            ans += word1[i];
-            i++;
+        if(i < word1.size()) {
+            mergedString += word1.substr(i, word1.size() - i);
         }
         
-        while(j < word2.size()) {
-            ans += word2[j];
-            j++;
+        if(j < word2.size()) {
+            mergedString += word2.substr(j, word2.size() - j);
         }
         
-        return ans;
+        return mergedString;
     }
 };
