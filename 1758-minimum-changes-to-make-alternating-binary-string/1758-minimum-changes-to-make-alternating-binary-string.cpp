ff3a9689aa4dcc710cef9b3575567dc88bd47793
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2023-12-24 15:51:17 +0600

    Time: 3 ms (78.34%), Space: 7.5 MB (12.76%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int count1 = 0, count2 = 0;
+        
+        for(int i = 0; i < s.size(); i++) {
+            if(i % 2 && s[i] != '0') {
+                count1++;
+            } else if(i % 2 == 0 && s[i] != '1') {
+                count1++;
+            }
+        }
+        
+        for(int i = 0; i < s.size(); i++) {
+            if(i % 2 && s[i] != '1') {
+                count2++;
+            } else if(i % 2 == 0 && s[i] != '0') {
+                count2++;
+            }
+        }
+        
+        return min(count1, count2);
+    }
+};
