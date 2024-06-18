6921f194f755655ecb38bdda458d72e097cf324c
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2024-06-18 21:48:59 +0600

    Time: 11 ms (37.12%), Space: 9.9 MB (5.90%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false;
+        
         unordered_map<char, int> mp;
+        
         for(int i = 0; i < s.size(); i++) {
             mp[s[i]]++;
             mp[t[i]]--;
         }
+        
         for(auto it = mp.begin(); it != mp.end(); it++) {
             if(it -> second != 0) return false;
         }
+        
         return true;
     }
 };
