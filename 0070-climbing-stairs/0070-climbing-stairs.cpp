f60fde4646deaff329323a827d0d0f7515812530
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2024-08-23 19:00:52 +0600

    Time: 0 ms (100.00%), Space: 7.4 MB (73.26%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int dp[50];
+    int dp[46];
     int climbStairs(int n) {
         memset(dp, 0, sizeof dp);
         return calculateWays(n);
     }
-    int calculateWays(int total) {
-        if(total <= 0) return total == 0;
-        if(dp[total] > 0) return dp[total];
-        dp[total] += calculateWays(total - 1) + calculateWays(total - 2);
-        return dp[total];
+    
+    int calculateWays(int n) {
+        if(dp[n]) return dp[n];
+        
+        if(n <= 1) return 1;
+        
+        return dp[n] = calculateWays(n - 1) + calculateWays(n - 2);
     }
 };
