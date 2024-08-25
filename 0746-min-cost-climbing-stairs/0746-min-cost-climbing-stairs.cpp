b85e0b66570b97a5b51c9ec2fbe4e4fe95ae6599
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2024-08-25 15:32:34 +0600

    Time: 0 ms (100.00%), Space: 16.7 MB (39.83%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    int memo[1001];
+    int dp[1001];
+    
     int minCostClimbingStairs(vector<int>& cost) {
-        memset(memo, 0, sizeof 0);
-        return min(findMinCost(cost, 0), findMinCost(cost, 1));
+        memset(dp, 0, sizeof dp);
+        return min(findMinCost(0, cost), findMinCost(1, cost));
     }
-    int findMinCost(vector<int>& cost, int i) {
-        if(i == cost.size() - 1) {
-            return cost[i];
-        }
-        if(i >= cost.size()) {
-            return 0;
-        }
-        if(memo[i] != 0) {
-            return memo[i];
-        }
-        memo[i] += cost[i] + min(findMinCost(cost, i + 1), findMinCost(cost, i + 2));
-        return memo[i];
+    
+    int findMinCost(int index, vector<int>& cost) {
+        if(dp[index]) return dp[index];
+        
+        if(index >= cost.size()) return 0;
+        
+        if(index == cost.size() - 1) return cost[index];
+        
+        return dp[index] += cost[index] + min(findMinCost(index + 1, cost), findMinCost(index + 2, cost));
     }
 };
-
-/*
-
-*/
