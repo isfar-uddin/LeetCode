fd2e2de9779b4e86d66c4b54e51991357b981b23
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2024-08-28 13:29:13 +0600

    Time: 0 ms (100.00%), Space: 9.5 MB (77.14%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int dp[2][101];
+    int dp[2][102];
+    
     int rob(vector<int>& nums) {
         if(nums.size() == 1) return nums[0];
         memset(dp, -1, sizeof dp);
-        return max(findMaxRobbery(nums, 0, 1), findMaxRobbery(nums, 1, 0));
+        return max(findMax(0, nums, 1), findMax(1, nums, 0));
     }
-    int findMaxRobbery(vector<int>& nums, int index, int isFirst) {
-        if(index >= nums.size() || (isFirst && index == nums.size() - 1)) {
-            return 0;
+    
+    int findMax(int index, vector<int>& nums, int isFirst) {
+        if(dp[isFirst][index] != -1) return dp[isFirst][index];
+        if(index >= nums.size()) return 0;
+        if(index == nums.size() - 1 && isFirst) {
+            if(isFirst) return 0;
         }
-        if(dp[isFirst][index] != -1) {
-            return dp[isFirst][index];
-        }
-        return dp[isFirst][index] = max(findMaxRobbery(nums, index + 1, isFirst), findMaxRobbery(nums, index + 2, isFirst) + nums[index]);
+        
+        return dp[isFirst][index] = max(findMax(index + 1, nums, isFirst), findMax(index + 2, nums, isFirst) + nums[index]);
     }
 };
