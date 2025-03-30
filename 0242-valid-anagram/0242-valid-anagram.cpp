833e42fa3d81913160658ee0257513bef0fa833c
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2025-03-30 19:36:07 +0600

    Time: 0 ms (100%), Space: 9.5 MB (91.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false;
-        
-        unordered_map<char, int> mp;
+
+        int letter[26];
 
         for(int i = 0; i < s.size(); i++) {
-            mp[s[i]]++;
-            mp[t[i]]--;
+            letter[s[i] - 'a']++;
+            letter[t[i] - 'a']--;
         }
 
-        for(auto i = mp.begin(); i != mp.end(); i++) {
-            if(i -> second != 0) return false;
+        for(int i = 0; i < 26; i++) {
+            if(letter[i] != 0) return false;
         }
 
         return true;
