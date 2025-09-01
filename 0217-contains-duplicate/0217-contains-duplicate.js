94ba9ba39a7bcee899db15a45f3a0fe4099923fd
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2025-09-01 23:58:26 +0600

    Time: 36 ms (24.59%), Space: 77.4 MB (18.87%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  const hashMap = {};
+
+  for(let i = 0; i < nums.length; i++) {
+    if(hashMap[nums[i]]) {
+      return true;
+    }
+
+    hashMap[nums[i]] = true;
+  }
+
+  return false;
+};
+
+/* 
+
+for( i = 0 -> n - 1)
+  for(j = i+ 1 -> n)
+    if(nums[i] == nums[j]) return true;
+return false
+
+TC -> O(n^2)
+SC -> O(1)
+
+map -> 
+for(i = 0 -> n - 1)
+  if(map[nums[i]]) return true;
+return false;
+
+TC -> O(n)
+SC -> O(n)
+ */
