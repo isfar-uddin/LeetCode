e43f439b2b41dcfac5dcaa5234f8eb53244259fa
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2025-09-02 01:25:54 +0600

    Time: 224 ms (5.21%), Space: 64.4 MB (5.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -12,6 +12,7 @@ var isAnagram = function(s, t) {
     const base = 'a'.charCodeAt(0);
 
     for(let i = 0; i < s.length; i++) {
+      console.log(s.charCodeAt(i) - base)
       arr[s.charCodeAt(i) - base]++;
       arr[t.charCodeAt(i) - base]--;
     }
@@ -26,7 +27,18 @@ Approach #1 ->
 TC -> O(m*n)
 SC -> O(1)
 
-Approach #2 if Unicode characters ->
+Approach #2 if Not unicode characters ->
+- store count on the arr size of 26
+- index should be 0 for a to 26 for z
+- increment count for char of s and decrement count for char of t
+- iterate through the arr if count is not equal to 0 return false
+- return true
+
+TC -> O(n)
+SC -> O(1)
+
+
+Approach #3 if Unicode characters ->
 - store count on the map
 - increment count for char of s and decrement count for char of t
 - iterate through the map if count is not equal to 0 return false
