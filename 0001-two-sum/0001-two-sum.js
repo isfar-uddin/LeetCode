051514a797e1956389732ad76048fb3c2e83b459
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2025-09-02 21:49:23 +0600

    Time: 2 ms (71.56%), Space: 55.2 MB (45.13%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const numsMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (numsMap.has(nums[i])) {
+      return [numsMap.get(nums[i]), i]
+    }
+    numsMap.set(target - nums[i], i);
+  }
+
+  return [];
+};
