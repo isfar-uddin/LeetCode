34ac2656eb0c0134bbc85af3ad2a7625a4c5f861
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2025-09-03 00:05:04 +0600

    Time: 2 ms (88.86%), Space: 63.9 MB (84%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.js
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.js
@@ -14,27 +14,28 @@
 var findTarget = function (root, k) {
   const set = new Set();
 
-  /* BFS implementation */
-
-  const queue = [];
-  queue.push(root);
-
-  while (queue.length) {
-    let length = queue.length;
-
-    while (length--) {
-      const node = queue.shift();
-
-      if (set.has(node.val)) return true;
-
-      set.add(k - node.val);
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
+  // BFS implementation
+  /* 
+    const queue = [];
+    queue.push(root);
+  
+    while (queue.length) {
+      let length = queue.length;
+  
+      while (length--) {
+        const node = queue.shift();
+  
+        if (set.has(node.val)) return true;
+  
+        set.add(k - node.val);
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+      }
     }
-  }
-
-  return false;
-
+  
+    return false;
+   */
+  // DFS approach
   const bst = node => {
     if (!node) return false;
     if (set.has(node.val)) return true;
