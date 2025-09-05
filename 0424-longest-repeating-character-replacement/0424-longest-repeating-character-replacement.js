097cdd5607874d983433553f85a12ed26910ee91
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2025-09-06 00:50:39 +0600

    Time: 16 ms (80.79%), Space: 56.4 MB (71.44%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -4,23 +4,24 @@
  * @return {number}
  */
 var characterReplacement = function (s, k) {
+  let maxFreq = 0, longestChar = 0, start = 0, end = 0;
   const frequency = new Map();
-  let start = 0, end = 0, longestString = 0, maxFreq = 0;
 
   while (end < s.length) {
-    const currFreq = (frequency.get(s[end]) || 0) + 1
+    const currFreq = frequency.get(s[end]) || 0;
     const currLength = end - start + 1;
-    frequency.set(s[end], currFreq);
-    maxFreq = Math.max(maxFreq, currFreq);
+    frequency.set(s[end], currFreq + 1);
+    maxFreq = Math.max(maxFreq, currFreq + 1);
 
-    if (currLength - maxFreq > k) {
+    if(currLength - maxFreq > k) {
       frequency.set(s[start], frequency.get(s[start]) - 1);
       start++;
     } else {
-      longestString = Math.max(longestString, currLength)
+      longestChar = Math.max(longestChar, currLength);
     }
+
     end++;
   }
 
-  return longestString;
+  return longestChar;
 };
