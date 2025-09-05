8ac679bca68257b9831332080949337f77dc82b7
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2025-09-06 05:07:34 +0600

    Time: 0 ms (100%), Space: 54.9 MB (10.96%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let start = 0, end = nums.length - 1;
+
+  while(start <= end) {
+    if(nums[start] === target) return start;
+    if(nums[end] === target) return end;
+
+    const mid = Math.floor(start + (end - start) / 2);
+
+    if(nums[mid] === target) {
+      return mid;
+    } else if(nums[mid] >= nums[start]) {
+      if(target < nums[mid] && target > nums[start]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    } else {
+      if(target > nums[mid] && target < nums[end]) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+  }
+
+  return - 1;
+};
