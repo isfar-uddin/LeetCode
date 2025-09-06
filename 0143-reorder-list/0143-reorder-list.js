e637ffd4e11121a7043c82d74f59d5bb38d9472d
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2025-09-06 18:31:10 +0600

    Time: 1 ms (92.53%), Space: 63.3 MB (99.17%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function (head) {
+  /* 
+    1. Reverse the last half
+    2. Then marge the last half to first half
+   */
+
+  let slow = head, fast = head?.next, prev = null;;
+
+  // Move to the pointer to the half of the list
+  while (fast?.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  // Reverse the last half
+  while (slow) {
+    let temp = slow.next;
+    slow.next = prev;
+    prev = slow;
+    slow = temp;
+  }
+
+  slow = head;
+  fast = prev;
+
+  while (slow && fast) {
+    let temp = slow.next;
+    slow.next = fast;
+    slow = fast;
+    fast = fast.next;
+    slow.next = temp;
+    slow = temp;
+  }
+
+  return head;
+};
