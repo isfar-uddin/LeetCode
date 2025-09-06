e5ce761ec4f4544fc0876a1b04d0b14f4ba3d219
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2025-09-06 17:24:27 +0600

    Time: 59 ms (21.74%), Space: 59.5 MB (33.72%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let slow = head, fast = head?.next;
+
+  while(fast?.next) {
+    if(slow === fast) {
+      return true;
+    }
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow === fast;
+};
