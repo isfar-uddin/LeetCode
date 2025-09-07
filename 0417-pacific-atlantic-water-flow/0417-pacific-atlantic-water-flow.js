396c3eb9749c3f1220d61ca720c83832ea3d55ef
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2025-09-07 17:41:55 +0600

    Time: 48 ms (31.41%), Space: 66.3 MB (45.16%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.js
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.js
@@ -3,50 +3,54 @@
  * @return {number[][]}
  */
 var pacificAtlantic = function (heights) {
-    const rowSize = heights.length;
-    const columnSize = heights[0].length;
-    const ans = [];
-    const pacific = Array.from({ length: rowSize }, () => Array.from({ length: columnSize }, () => false));
-    const atlantic = Array.from({ length: rowSize }, () => Array.from({ length: columnSize }, () => false));
+  const rowSize = heights.length;
+  const columnSize = heights[0].length;
+  const pacific = Array.from({ length: rowSize }, () => Array.from({ length: columnSize }, () => false));
+  const atlantic = Array.from({ length: rowSize }, () => Array.from({ length: columnSize }, () => false));
 
-    const dfs = (row, column, ocean) => {
-        if (ocean[row][column] || row < 0 || row >= rowSize || column < 0 || column >= columnSize) return;
+  const backtrack = (i, j, ocean) => {
+    if (i < 0 || j < 0 || i >= rowSize || j >= columnSize || ocean[i][j]) return;
 
-        ocean[row][column] = true;
-        if (row + 1 < rowSize && heights[row + 1][column] >= heights[row][column]) {
-            dfs(row + 1, column, ocean);
-        }
-        if (row - 1 >= 0 && heights[row - 1][column] >= heights[row][column]) {
-            dfs(row - 1, column, ocean);
-        }
-        if (column + 1 < columnSize && heights[row][column + 1] >= heights[row][column]) {
-            dfs(row, column + 1, ocean);
-        }
-        if (column - 1 >= 0 && heights[row][column - 1] >= heights[row][column]) {
-            dfs(row, column - 1, ocean);
-        }
+    ocean[i][j] = true;
 
+    if (heights[i + 1]?.[j] >= heights[i][j]) {
+      backtrack(i + 1, j, ocean);
     }
 
-    for (let i = 0; i < rowSize; i++) {
-        for (let j = 0; j < columnSize; j++) {
-            if (i == 0 || j == 0) {
-                dfs(i, j, pacific);
-            }
-
-            if (i == rowSize - 1 || j == columnSize - 1) {
-                dfs(i, j, atlantic);
-            }
-        }
+    if (heights[i - 1]?.[j] >= heights[i][j]) {
+      backtrack(i - 1, j, ocean);
     }
 
-    for (let i = 0; i < rowSize; i++) {
-        for (let j = 0; j < columnSize; j++) {
-            if (pacific[i][j] && atlantic[i][j]) {
-                ans.push([i, j])
-            }
-        }
+    if (heights[i]?.[j + 1] >= heights[i][j]) {
+      backtrack(i, j + 1, ocean);
     }
 
-    return ans;
+    if (heights[i]?.[j - 1] >= heights[i][j]) {
+      backtrack(i, j - 1, ocean);
+    }
+  }
+
+  for (let i = 0; i < rowSize; i++) {
+    for (let j = 0; j < columnSize; j++) {
+      if (i == 0 || j == 0) {
+        backtrack(i, j, pacific);
+      }
+
+      if (i == rowSize - 1 || j == columnSize - 1) {
+        backtrack(i, j, atlantic);
+      }
+    }
+  }
+
+  const ans = [];
+
+  for (let i = 0; i < rowSize; i++) {
+    for (let j = 0; j < columnSize; j++) {
+      if (pacific[i][j] && atlantic[i][j]) {
+        ans.push([i, j]);
+      }
+    }
+  }
+
+  return ans;
 };
