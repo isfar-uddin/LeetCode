2b6534fa2ec1f7e2f7b694380ffc8fae78b74515
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2025-09-09 04:05:49 +0600

    Time: 20 ms (70.8%), Space: 56.6 MB (69.33%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -6,31 +6,21 @@ var longestPalindrome = function (s) {
   let ans = "";
   let maxLen = 0;
 
+  const expandAroundCenter = (left, right) => {
+    while (left >= 0 && right < s.length && s[left] == s[right]) {
+      let currLen = right - left + 1;
+      if (currLen > maxLen) {
+        maxLen = currLen;
+        ans = s.slice(left, left + maxLen);
+      }
+      left--;
+      right++;
+    }
+  }
+
   for (let i = 0; i < s.length; i++) {
-    let left = i;
-    let right = i;
-
-    while (left >= 0 && right < s.length && s[left] == s[right]) {
-      let currLen = right - left + 1;
-      if (currLen > maxLen) {
-        maxLen = currLen;
-        ans = s.slice(left, left + maxLen);
-      }
-      left--;
-      right++;
-    }
-
-    left = i, right = i + 1;
-
-    while (left >= 0 && right < s.length && s[left] == s[right]) {
-      let currLen = right - left + 1;
-      if (currLen > maxLen) {
-        maxLen = currLen;
-        ans = s.slice(left, left + maxLen);
-      }
-      left--;
-      right++;
-    }
+    expandAroundCenter(i, i);
+    expandAroundCenter(i, i + 1);
   }
 
   return ans;
