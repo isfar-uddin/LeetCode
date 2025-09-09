699aea95cdbef5e0d921a1a72485e5350c411511
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2025-09-09 06:04:25 +0600

    Time: 8 ms (92.28%), Space: 60.5 MB (91.62%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -3,44 +3,40 @@
  * @param {number[][]} prerequisites
  * @return {boolean}
  */
-var canFinish = function(numCourses, prerequisites) {
-    let preMap = {};
-    let visited = {};
+var canFinish = function (numCourses, prerequisites) {
+  const preMap = {};
+  const visited = {};
 
-    function dfs(currNode) {
-        if(visited[currNode]) {
-            return false;
-        }
+  const dfs = (node) => {
+    if (visited[node]) return false;
+    visited[node] = true;
 
-        visited[currNode] = true;
-
-        for(let i = 0; i < preMap[currNode]?.length; i++) {
-            if(!dfs(preMap[currNode][i])) {
-                return false;
-            }
-        }
-
-        visited[currNode] = false;
-        preMap[currNode] = [];
-
-        return true;
-
+    for (let i = 0; i < preMap[node]?.length; i++) {
+      const currNode = preMap[node][i];
+      if (!dfs(currNode)) {
+        return false;
+      }
     }
-
-    for(let i = 0; i < prerequisites.length; i++) {
-        const currItem = prerequisites[i];
-        if(preMap[currItem[0]] == undefined) {
-            preMap[currItem[0]] = [currItem[1]];
-        } else {
-            preMap[currItem[0]].push(currItem[1]);
-        }
-    }
-
-    for(let key in preMap) {
-        if(!dfs(key)) {
-            return false;
-        }
-    }
-
+    visited[node] = false;
+    preMap[node] = [];
     return true;
+  }
+
+  for (let i = 0; i < prerequisites.length; i++) {
+    const course = prerequisites[i];
+
+    if (preMap[course[0]] === undefined) {
+      preMap[course[0]] = [course[1]];
+    } else {
+      preMap[course[0]].push(course[1]);
+    }
+  }
+
+  for(const key in preMap) {
+    if(!dfs(key)) {
+      return false;
+    }
+  }
+
+  return true;
 };
