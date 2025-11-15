58f9152102f54aa5b992be024066051385985aac
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2025-11-15 13:31:25 +0600

    Time: 2 ms (68.93%), Space: 63.9 MB (68.93%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -2,16 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function (nums) {
-  let maxSum = -Infinity, sum = 0;
+var maxSubArray = function(nums) {
+    let maxSum = Number.NEGATIVE_INFINITY;
+    let currSum = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    sum += nums[i];
-    maxSum = Math.max(maxSum, sum);
-    if (sum < 0) {
-      sum = 0;
+    for(let i = 0; i < nums.length; i++) {
+      currSum += nums[i];
+      maxSum = Math.max(maxSum, currSum);
+
+      if(currSum < 0) {
+        currSum = 0;
+      }
     }
-  }
 
-  return maxSum;
+    return maxSum;
 };
