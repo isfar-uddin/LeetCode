64e84c3529d3981f782f788584489a7eea38808e
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2026-09-22 15:52:57 +0600

    Time: 91 ms (31.17%), Space: 53.8 MB (90.89%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+    const n = nums.length;
+    const dp = Array.from({ length: n }).fill(1);
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1)
+            }
+        }
+    }
+
+    return Math.max(...dp);
+};
