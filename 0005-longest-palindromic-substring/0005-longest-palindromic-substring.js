67f1fc3c9c5cb3028b5a78e245f168033fffba67
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2026-09-22 13:32:13 +0600

    Time: 624 ms (8.96%), Space: 92.9 MB (5.21%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -3,25 +3,36 @@
  * @return {string}
  */
 var longestPalindrome = function (s) {
-  let ans = "";
-  let maxLen = 0;
+    const length = s.length;
+    let start = 0;
+    let maxLen = 1;
+    const dp = Array.from({ length }, () => new Array(length).fill(false));
 
-  const expandAroundCenter = (left, right) => {
-    while (left >= 0 && right < s.length && s[left] == s[right]) {
-      let currLen = right - left + 1;
-      if (currLen > maxLen) {
-        maxLen = currLen;
-        ans = s.slice(left, left + maxLen);
-      }
-      left--;
-      right++;
+    for (let i = 0; i < length; i++) {
+        dp[i][i] = true;
     }
-  }
 
-  for (let i = 0; i < s.length; i++) {
-    expandAroundCenter(i, i);
-    expandAroundCenter(i, i + 1);
-  }
+    for (let i = 0; i < length - 1; i++) {
+        if (s[i] == s[i + 1]) {
+            dp[i][i + 1] = true;
+            maxLen = 2;
+            start = i;
+        }
+    }
 
-  return ans;
+    for (let len = 3; len <= length; len++) {
+        for (let i = 0; i <= length - len; i++) {
+            let j = len + i - 1;
+            if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                dp[i][j] = true;
+
+                if (len > maxLen) {
+                    start = i;
+                    maxLen = len;
+                }
+            }
+        }
+    }
+
+    return s.substr(start, maxLen);
 };
