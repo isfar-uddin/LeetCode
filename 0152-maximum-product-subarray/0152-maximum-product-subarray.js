bf1f42629da92338dc40fe86d5eca840e6879de3
Md. Isfar Uddin <31846429+isfar-uddin@users.noreply.github.com>
2026-09-22 12:36:55 +0600

    Time: 1 ms (90.76%), Space: 54.4 MB (73.49%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -3,15 +3,18 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    let max = Number.NEGATIVE_INFINITY, prevMax = 1, prevMin = 1;
+    let result = Number.NEGATIVE_INFINITY;
+    let prevMax = 1;
+    let prevMin = 1;
 
     for(let i = 0; i < nums.length; i++) {
-      let currMax = Math.max(nums[i], prevMax * nums[i], prevMin * nums[i]);
-      let currMin = Math.min(nums[i], prevMax * nums[i], prevMin * nums[i]);
-      prevMax = currMax;
-      prevMin = currMin;
-      max = Math.max(currMax, max);
+        const currMax = Math.max(nums[i], prevMax * nums[i], prevMin * nums[i]);
+        const currMin = Math.min(nums[i], prevMax * nums[i], prevMin * nums[i]);
+        prevMax = currMax;
+        prevMin = currMin;
+
+        result = Math.max(result, currMax)
     }
 
-    return max;
+    return result;
 };
